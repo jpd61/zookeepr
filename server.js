8eea20844e21d4af53d7cb748107d8b24211c6af
jpd61 <jpd@dewoodyoil.com>
2020-05-24 13:14:56 -0500

    fixed fat fingers

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
 
-    if (!validateAnumal(req.body)) {
+    if (!validateAnimal(req.body)) {
         res.status(400).send('The animal is not properly formatted.');
     } else {
         const animal = createNewAnimal(req.body, animals);
