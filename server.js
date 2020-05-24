5632648e20c21f8690c898d5b140db855422c4aa
jpd61 <jpd@dewoodyoil.com>
2020-05-24 12:25:03 -0500

    updated id and param

--- server.js
+++ server.js
@@ -39,14 +39,28 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
 }
 
 app.get('/api/animals', (req, res) => {
-    let results = animals;
+    let result = animals;
     if (req.query) {
-        results = filterByQuery(req.query, results);
+        result = filterByQuery(req.query, result);
     }
-    res.json(results);
+    res.json(result);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
